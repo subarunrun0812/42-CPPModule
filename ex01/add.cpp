bd6580fabde83bc715f205cc8ba2ff3e77c81086
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-06-07 13:10:44 +0900

    add:TODOを追加

--- ex01/add.cpp
+++ ex01/add.cpp
@@ -13,36 +13,37 @@ void PhoneBook::ft_add(void)
     
     std::string firstName;
     std::cout << "Input" << std::endl;
+    //TODO: std::cinでやるとめっちゃ大変
     std::cout << "first name    : " && std::cin >> firstName;
     //TODO:emptyの条件式に入らない
-    if (firstName.empty())
-    {
-        std::cout << "から" << std::endl;
-        empty_error();
-    }
+    // if (firstName.empty() || firstName.eof)
+    // {
+    //     std::cout << "から" << std::endl;
+    //     empty_error();
+    // }
     setFirstName(firstName);
 
     std::string lastName;
     std::cout << "last name     : " && std::cin >> lastName;
-    if (lastName.empty())
-        empty_error();
+    // if (lastName.empty())
+    //     empty_error();
     setLastName(lastName);
 
     std::string nickName;
     std::cout << "nick name     : " && std::cin >> nickName;
-    if (nickName.empty())
-        empty_error();
+    // if (nickName.empty())
+    //     empty_error();
     setNickName(nickName);
 
     std::string phoneNumber;
     std::cout << "phone number  : " && std::cin >> phoneNumber;
-    if (phoneNumber.empty())
-        empty_error();
+    // if (phoneNumber.empty())
+    //     empty_error();
     setPhoneNumber(phoneNumber);
 
     std::string darkestSecret;
     std::cout << "darkest secret: " && std::cin >> darkestSecret;
-    if (darkestSecret.empty())
-        empty_error();
+    // if (darkestSecret.empty())
+    //     empty_error();
     setDarkestSecret(darkestSecret);
 }
