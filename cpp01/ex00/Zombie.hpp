29a1f599dc28127f3c770564bea715973b3e0566
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-06-12 11:26:56 +0900

    add: ex00

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,24 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+// 
+class Zombie
+{
+	private:
+		void announce(void);
+	public:
+
+}
+
+void randomChump( std::string name )
+{
+
+}
+
+void randomChump( std::string name );
+
+Zombie* newZombie( std::string name );
---
+++ cpp01/ex00/newZombie.cpp
@@ -0,0 +1,7 @@
+#include "Zombie.hpp"
+
+// クラス型のオブジェクトのポインタ
+Zombie* newZombie( std::string name )
+{
+	std::cout << "Foo: BraiiiiinnnzzzZ..." << std::endl;
+}
