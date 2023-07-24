e6a2a4c6192599e630ba13f9c4927c3ba34e69c2
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-07-24 17:23:35 +0900

    commit

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -3,22 +3,22 @@
 
 void Harl::debug()
 {
-	std::cout << "\x1b[32mDebug message: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n\x1b[0m";
+	std::cout << "\x1b[32m[Debug] I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n\x1b[0m";
 }
 
 void Harl::info()
 {
-	std::cout << "\x1b[32mInfo message: I cannot believe adding extra bacon costs more money. You didn\'t put enough bacon in my burger!\n\x1b[0m";
+	std::cout << "\x1b[32m[Info] I cannot believe adding extra bacon costs more money. You didn\'t put enough bacon in my burger!\n\x1b[0m";
 }
 
 void Harl::warning()
 {
-	std::cout << "\x1b[33mWarning message: I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n\x1b[0m";
+	std::cout << "\x1b[33m[Warning] I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n\x1b[0m";
 }
 
 void Harl::error()
 {
-	std::cout << "\x1b[31mError message: This is unacceptable! I want to speak to the manager now.\n\x1b[0m";
+	std::cout << "\x1b[31m[Error] This is unacceptable! I want to speak to the manager now.\n\x1b[0m";
 }
 
 void Harl::complain(std::string level)
