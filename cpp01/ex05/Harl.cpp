7c546a76bfadecfbe2da7fc8c6c74070f86dd4e0
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-07-24 17:23:15 +0900

    add: cpp01

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,44 @@
+#include "Harl.hpp"
+#include <map>
+
+void Harl::debug()
+{
+	std::cout << "\x1b[32mDebug message: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n\x1b[0m";
+}
+
+void Harl::info()
+{
+	std::cout << "\x1b[32mInfo message: I cannot believe adding extra bacon costs more money. You didn\'t put enough bacon in my burger!\n\x1b[0m";
+}
+
+void Harl::warning()
+{
+	std::cout << "\x1b[33mWarning message: I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n\x1b[0m";
+}
+
+void Harl::error()
+{
+	std::cout << "\x1b[31mError message: This is unacceptable! I want to speak to the manager now.\n\x1b[0m";
+}
+
+void Harl::complain(std::string level)
+{
+	// このマップのキーはstd::string型で、値はHarlクラスのメンバ関数を指す関数ポインタ
+	std::map<std::string, void (Harl::*)()> levelFuncs;
+	levelFuncs["DEBUG"] = &Harl::debug;
+	levelFuncs["INFO"] = &Harl::info;
+	levelFuncs["WARNING"] = &Harl::warning;
+	levelFuncs["ERROR"] = &Harl::error;
+
+	if (levelFuncs.count(level) > 0)
+	{
+		//対応するメンバ関数を呼び出しています。関数ポインタを使用して、
+		//levelに対応するHarlクラスのメンバ関数を呼び出しています。
+		//例えば、levelが"DEBUG"であれば、Harl::debug関数が呼び出されます。
+		(this->*levelFuncs[level])();
+	}
+	else
+	{
+		std::cout << "Invalid level!\n";
+	}
+};
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Harl.hpp"
+
+int main() {
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+
+    return 0;
+}
