87175bb9fbaadd7f32a213b5595d71c16b2e89eb
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-08-10 12:21:00 +0900

    add: メモリリークチェックのコードをmain.cに記述

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -5,4 +5,11 @@ int main(void)
     Zombie *zombie = newZombie("Foo");
     zombie->announce();
 	randomChump("Random");
+    delete zombie;
+
 }
+
+// #include <stdlib.h>
+// __attribute__((destructor)) static void destructor(void) {
+// 	system("leaks -q ex00");
+// }
