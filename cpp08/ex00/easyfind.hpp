44d74f6cf02fcdf197f4187c89362571fb5be67b
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-09-04 22:22:39 +0900

    push

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define NORMAL "\x1b[0m"
+#define LINE "=========="
+#define LENGTH 4
+
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+class EasyFind
+{
+    private:
+
+    public:
+        // EasyFind(T _t, int num);
+}
+
+#endif
