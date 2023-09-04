5204f62693bf66218c400d3a1e9c3277e5e8cb72
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-09-04 12:14:47 +0900

    add:ex01 3つのparametorを受け取るiter()を作成した

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,17 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define NORMAL "\x1b[0m"
+#define LINE "=========="
+
+#include <iostream>
+
+template <typename T>
+void iter(T* arr, T num, void* func(T&))
+{
+
+}
+
+#endif
