2316e24e2460b967ce9b08d7f588ccd86f1c3c61
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-09-07 13:07:59 +0900

    cpp09/ex02の課題概要を記述した

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,32 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define NORMAL "\x1b[0m"
+#define LINE "=========="
+#define SUCCESS 0
+#define FAILE 1
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <list>
+
+/*
+TODO:
+引数として、正の整数を複数受け取る
+それらをソートするために、マージインサートソードアルゴリズムを使用する
+Ford-Johson algorithm
+プログラム実行中にエラーが発生したら、エラーメッセージを表示する。
+3000種類の正の整数が来ても、ソートできるようにする
+出力:
+1行目=ソートされていない整数を表示
+2行目=ソートされた整数を表示
+3行目=使用したコンテナとアルゴリズムの計算時間を表示
+4行目=使用したコンテナとアルゴリズムの計算時間を表示
+
+
+*/
+
+#endif
